b085996536f4ff4ca85c32d80e9a6b26a98c4ed9
r0jjames <rojjamescarranza@gmail.com>
2024-04-21 11:13:49 +0800

    Add mapper class

--- Java-Project/accounts/src/main/java/com/project/accounts/mapper/AccountsMapper.java
+++ Java-Project/accounts/src/main/java/com/project/accounts/mapper/AccountsMapper.java
@@ -1,8 +1,20 @@
 package com.project.accounts.mapper;
 
 import com.project.accounts.dto.AccountsDto;
+import com.project.accounts.entity.Accounts;
 
 public class AccountsMapper {
 
-//    public static AccountsDto mapToAccountsDto(Acc)
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
 }
---
+++ Java-Project/accounts/src/main/java/com/project/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,19 @@
+package com.project.accounts.mapper;
+
+import com.project.accounts.dto.CustomerDto;
+import com.project.accounts.entity.Customer;
+
+public class CustomerMapper {
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
