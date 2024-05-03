876b863a57654919fb7c57f09b177dd23c865783
r0jjames <rojjamescarranza@gmail.com>
2024-05-03 23:41:15 +0200

    Loans Microservice

--- java-microservices/loans/src/main/java/com/project/loans/entity/Loans.java
+++ java-microservices/loans/src/main/java/com/project/loans/entity/Loans.java
@@ -9,15 +9,22 @@ import lombok.*;
 
 @Entity
 @Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
-public class Loans {
+public class Loans extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long loanId;
+
     private String mobileNumber;
+
     private String loanNumber;
+
     private String loanType;
-    private int totalAmount;
+
+    private int totalLoan;
+
     private int amountPaid;
+
     private int outstandingAmount;
+
 }
