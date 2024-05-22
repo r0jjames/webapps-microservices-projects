fa4d85670df4f31a13aa6250f706b72d12bd6c0b
r0jjames <rojjamescarranza@gmail.com>
2024-05-22 22:49:49 +0200

    Custom Route using Spring Cloud Gateway

--- java-microservices/gatewayserver/src/main/java/com/project/gatewayserver/GatewayserverApplication.java
+++ java-microservices/gatewayserver/src/main/java/com/project/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,38 @@ package com.project.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+/*	example usage:
+	http://localhost:8072/rudzbank/accounts/api/create
+	http://localhost:8072/rudzbank/cards/api/create
+	http://localhost:8072/rudzbank/loans/api/create
+*/
+	@Bean
+	public RouteLocator customRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/rudzbank/accounts/**")
+						.filters(f -> f.rewritePath("/rudzbank/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/rudzbank/cards/**")
+						.filters(f -> f.rewritePath("/rudzbank/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.route(p -> p
+						.path("/rudzbank/loans/**")
+						.filters(f -> f.rewritePath("/rudzbank/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS")).build();
+
 	}
 
 }
