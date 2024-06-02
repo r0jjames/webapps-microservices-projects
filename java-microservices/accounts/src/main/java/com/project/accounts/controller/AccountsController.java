f02186aabb5cdb22505a676c4b786f542419d8c1
r0jjames <rojjamescarranza@gmail.com>
2024-06-02 23:08:27 +0200

    Implement Retry Pattern in accounts

--- java-microservices/accounts/src/main/java/com/project/accounts/controller/AccountsController.java
+++ java-microservices/accounts/src/main/java/com/project/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.project.accounts.dto.CustomerDto;
 import com.project.accounts.dto.ErrorResponseDto;
 import com.project.accounts.dto.ResponseDto;
 import com.project.accounts.service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,6 +15,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -34,15 +37,16 @@ import org.springframework.web.bind.annotation.*;
 
 public class AccountsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
     private final IAccountsService iAccountsService;
 
     public AccountsController(IAccountsService iAccountsService) {
         this.iAccountsService = iAccountsService;
     }
 
-//    @Value("${build.version}")
-//    private String buildVersion;
-//
+    @Value("${build.version}")
+    private String buildVersion;
+
     @Autowired
     private Environment environment;
 
@@ -175,30 +179,40 @@ public class AccountsController {
         }
     }
 
-//    @Operation(
-//            summary = "Get Build information",
-//            description = "Get Build information that is deployed into accounts microservice"
-//    )
-//    @ApiResponses({
-//            @ApiResponse(
-//                    responseCode = "200",
-//                    description = "HTTP Status OK"
-//            ),
-//            @ApiResponse(
-//                    responseCode = "500",
-//                    description = "HTTP Status Internal Server Error",
-//                    content = @Content(
-//                            schema = @Schema(implementation = ErrorResponseDto.class)
-//                    )
-//            )
-//    }
-//    )
-//    @GetMapping("/build-info")
-//    public ResponseEntity<String> getBuildInfo() {
+    @Operation(
+            summary = "Get Build information",
+            description = "Get Build information that is deployed into accounts microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @Retry(name="getBuildInfo", fallbackMethod = "getBuildInfoFallback")
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        logger.debug("getBuildInfo method invoked");
+        throw new NullPointerException();
 //        return ResponseEntity
 //                .status(HttpStatus.OK)
 //                .body(buildVersion);
-//    }
+    }
+
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+        logger.debug("getBuildInfoFallback method invoked");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("0.9");
+    }
 
     @Operation(
             summary = "Get Java version",
