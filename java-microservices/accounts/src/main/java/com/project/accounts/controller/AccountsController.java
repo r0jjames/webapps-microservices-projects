440d0c89e88dbddffa401f000019d26a22ecdbe8
r0jjames <rojjamescarranza@gmail.com>
2024-06-03 10:15:19 +0200

    clean up and fix bugs
    - Add exceptions
    - Remove header during retry
    - Revert changes in buildInfo API

--- java-microservices/accounts/src/main/java/com/project/accounts/controller/AccountsController.java
+++ java-microservices/accounts/src/main/java/com/project/accounts/controller/AccountsController.java
@@ -201,10 +201,9 @@ public class AccountsController {
     @GetMapping("/build-info")
     public ResponseEntity<String> getBuildInfo() {
         logger.debug("getBuildInfo method invoked");
-        throw new NullPointerException();
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body(buildVersion);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
     }
 
     public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
--- java-microservices/gatewayserver/src/main/java/com/project/gatewayserver/filter/ResponseTraceFilter.java
+++ java-microservices/gatewayserver/src/main/java/com/project/gatewayserver/filter/ResponseTraceFilter.java
@@ -22,8 +22,10 @@ public class ResponseTraceFilter {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
                 String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                logger.debug("updated the correlation id to the outbound headers: {}", correlationId);
-                exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+                if(exchange.getResponse().getHeaders().containsKey(FilterUtility.CORRELATION_ID)) {
+                    logger.debug("updated the correlation id to the outbound headers: {}", correlationId);
+                    exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+                }
             }));
         };
     }
