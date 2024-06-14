400e7000747bd269bbd8d4f80d80d6aed116747b
r0jjames <rojjamescarranza@gmail.com>
2024-06-14 10:09:43 +0200

    Making Kubernetes Discovery Client
    - Remove Eureka Server as Service Discovery
    - Use Kubernetes to handle the Service Discovery

--- java-microservices/loans/src/main/java/com/project/loans/LoansApplication.java
+++ java-microservices/loans/src/main/java/com/project/loans/LoansApplication.java
@@ -9,9 +9,11 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 /*@ComponentScans({ @ComponentScan("com.rudzbank.loans.controller") })
 @EnableJpaRepositories("com.eazybytes.loans.repository")
